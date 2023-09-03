7bff1e6600d33a1a8441f59b3b3d4cd3ab9d8946
Surieya <surieyasurieya@gmail.com>
2023-09-03 23:12:40 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import TechnicalEvents from './components/TechEvents';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Events from './pages/Events';
-import ContactUs from './pages/ContactUs';
-import SingleEventPage from './components/SingleEvent';
-import AllEvents from './pages/AllEvents';
-import './App.css'
-
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+// import Events from './pages/Events';
+import ContactUs from "./pages/ContactUs";
+import SingleEventPage from "./components/SingleEvent";
+import AllEvents from "./pages/AllEvents";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='app flex-row bg-cover bg-center' >
-      
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/events" element={<AllEvents />} />
-              <Route path="/events/:id" element={<SingleEventPage/>}/>
-              <Route path="/about" element={ <AboutUs/>} />
-              <Route path="/events" element={<Events/>} />
-              <Route path="/contactus" element={<ContactUs/>} />
-            </Routes>
-          </Router>
+    <div className="app flex-row bg-cover bg-center">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<AllEvents />} />
+          <Route path="/events/:id" element={<SingleEventPage />} />
+          <Route path="/about" element={<AboutUs />} />
+          {/* <Route path="/contactus" element={<ContactUs/>} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,19 @@
-import Navbar from '/src/components/Navbar';
-import Home1 from '../components/Home';
+import Navbar from "/src/components/Navbar";
+import Home1 from "../components/Home";
 import "./home.css";
-import cd from '../assets/cd.jpg';
-
+import cd from "../assets/cd.jpg";
 
 const Home = () => {
   return (
-    <div className='bgg op-0 h-screen bg-fixed '>
-      
-      <div className=' fixed w-auto md:h-screen md:w-full '>
+    <div className="bgg op-0 h-screen bg-fixed ">
+      {/* <div className=' fixed w-auto md:h-screen md:w-full '>
         <Navbar/>
+      </div> */}
+      <div>
+        <Home1 />
       </div>
-      <div >
-        <Home1/>
-      </div>
-      
     </div>
-    );
-}
+  );
+};
 //  border-b border-gray-600
 export default Home;
