b9790b04011d5fec772fc3343ff4b619e9550334
josephpeter231 <josephpeterjcit@gmail.com>
2023-09-03 19:32:00 +0530

    modified home page final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,76 @@
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import AnimateVariant from './framer';
 import Footer from './footer';
 import Section from './Section';
 import Memories from './Memories';
 import ContactUS from './ContactUs';
-import Aboutus1 from "../components/About";
 import CardWithVideo from './Memories1';
 
 const Home1 = () => {
-    return (
-      <motion.div className="text-white flex-grow h-screen md:h-screen"
+  const targetDate = new Date('2023-09-21T00:00:00').getTime();
+  const [countdown, setCountdown] = useState(calculateCountdown());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const newCountdown = calculateCountdown();
+      setCountdown(newCountdown);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
+
+  function calculateCountdown() {
+    const now = new Date().getTime();
+    const timeDifference = targetDate - now;
+
+    if (timeDifference <= 0) {
+      return 'Countdown expired';
+    }
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  }
+
+  return (
+    <motion.div
+      className="text-white flex-grow h-screen md:h-screen"
       initial="offscreen"
       animate="onscreen"
       variants={AnimateVariant}
       viewport={{ once: true, amount: 0.8 }}
-
-    //   style={{WebkitBackdropFilter: 'blur(5px)', // Apply the blur effect using -webkit-backdrop-filter
-    // backdropFilter: 'blur(5px)'}}
-      >
-
-        <div className='flex flex-col items-center w-full h-screen pt-24'>
-         
-         <div className="mt-24 rounded-lg justify-center p-3" style={{WebkitBackdropFilter: 'blur(3px)', // Apply the blur effect using -webkit-backdrop-filter
-    backdropFilter: 'blur(3px)'}} >           
-             <h1 className="text-center text-3xl md:text-5xl " >VIDYUTRENZ</h1>
-             <p className="font-serif text-center text-xs md:text-base  text-white text-opacity-40">
-                National Level Technical Symposiyum from the Department of Electronics and Communication Engineering
-            </p>
-        </div> 
-         <div className="mt-[250px] md:mt-[370px]">
-            <Footer/>
+    >
+      <div className='flex flex-col items-center w-full h-screen pt-24'>
+        <div
+          className="mt-24 rounded-lg justify-center p-3"
+          style={{
+            WebkitBackdropFilter: 'blur(3px)',
+            backdropFilter: 'blur(3px)',
+          }}
+        >
+          <h1 className="text-center text-3xl md:text-6xl">VIDYUTRENZ</h1>
+          <p className="font-serif text-center text-s md:text-base  text-white text-opacity-40">
+            National Level Technical Symposium from the Department of Electronics and Communication Engineering
+          </p>
+          <div>
+            <p className="mt-8 text-center text-2xl"> {countdown}</p>
+          </div>
         </div>
-            <Aboutus1/>
-            <Section/>
-            <Memories/>
-            <ContactUS/>
-            <Footer/> 
+        <div className="mt-[160px] md:mt-[220px] md:ml-6">
+          <Footer />
         </div>
-        
+        <Section />
+        <Memories />
+        <CardWithVideo />
+        <ContactUS />
+        <Footer />
+      </div>
+    </motion.div>
+  );
+}
 
-      </motion.div>
-      );
-  }
-   
-  export default Home1;
+export default Home1;
